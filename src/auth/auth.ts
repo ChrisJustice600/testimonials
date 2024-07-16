34d2bf57a1cd5fe2fa93870bd1bc817a9d80038e
unknown <christianmabeladi@gmail.com>
2024-07-16 22:14:13 +0100

    add env typesafe

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,9 +1,14 @@
+import { env } from "@/env";
 import prisma from "@/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
-
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Google],
+  providers: [
+    Google({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
 });
